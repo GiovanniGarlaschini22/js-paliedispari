2c8f37f5c45923d8375fd25aa2b8ab89fb141508
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-04-22 18:13:57 +0200

    Feat: funzione di verifica se numero é pari

--- PARIEDISPARI/js/script-pariedispari.js
+++ PARIEDISPARI/js/script-pariedispari.js
@@ -2,3 +2,8 @@
 function generaNumeroCasuale() {
     return Math.floor(Math.random() * 5) + 1;
 }
+
+// Funzione di verifica se numero é pari //
+function numPari(numero) {
+    return numero % 2 === 0;
+}
